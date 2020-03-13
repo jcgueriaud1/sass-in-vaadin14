0f87a7c6d07195b760e5b74b634f1928500e6ea0
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2020-03-13 14:18:52 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Example of SASS file used in Vaadin 14
+
+The SASS files are loaded with webpack.
+
+If you want to reproduce in an other project:
+* Install sass-loader and node-sass: npm install node-sass scss-loader
+    * This will add these dependencies to package.json
+* Update the webpack configuration (webpack.config.js):
+module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [ "raw-loader", "sass-loader"],
+            }
+        ]
+    }, 
+    
+
+You can import your SCSS with @CssImport annotation.
+If you modify your scss file and reload the page the new style should be applied.
---
+++ src/main/java/org/vaadin/jeanchristophe/MainView.java
@@ -0,0 +1,52 @@
+package org.vaadin.jeanchristophe;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route("")
+@PWA(name = "Project Base for Vaadin", shortName = "Project Base")
+@CssImport("./styles/scss/shared-styles.scss")
+@CssImport(value = "./styles/scss/vaadin-text-field-styles.scss", themeFor = "vaadin-text-field")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        // Use TextField for standard text input
+        Div div = new Div();
+        TextField textField = new TextField("Your name");
+
+        // Button click listeners can be defined as lambda expressions
+        GreetService greetService = new GreetService();
+        Button button = new Button("Say hello",
+                e -> Notification.show(greetService.greet(textField.getValue())));
+
+        // Theme variants give you predefined extra styles for components.
+        // Example: Primary button is more prominent look.
+        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        // You can specify keyboard shortcuts for buttons.
+        // Example: Pressing enter in this view clicks the Button.
+        button.addClickShortcut(Key.ENTER);
+
+        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
+        div.addClassName("centered-content");
+
+        div.add(textField, button);
+
+        div.add(new Paragraph("Inside centered-content"));
+
+        add(div, new Paragraph("Outside centered-content"));
+    }
+}
